8a011f29fb2c53d6886ffd521199cab8595c986b
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-09-23 11:13:02 -0500

    Created the beginning of game

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class AdventureGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome Warrior! what is your name?");
+        String charName = scan.nextLine();
+//        System.out.println("charName = " + charName);
+        System.out.println("Would you like to go on an adventure " + charName + "?");
+        String yesOrNo = scan.nextLine();
+
+        if (yesOrNo.equalsIgnoreCase("y")) {
+            System.out.println("Awesome sauce! Here is your first challenge!");
+        } else {
+            System.out.println("FATALITY!!!");
+        }
+     }
 }
