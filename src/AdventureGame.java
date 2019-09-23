cf979d51bacad460f687301cff7980aa016001ff
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-09-23 11:47:44 -0500

    battle system;

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -14,16 +14,43 @@ public class AdventureGame {
             //entering betsy stats
             System.out.println("Betsy the Spider\n");
             int attackPoints = 8;
-            int hp = 50;
+            int betsyHp = 50;
             System.out.println("Betsy's attack is " + attackPoints + ".\n");
-            System.out.println("Betsy's Hitpoints are " + hp + ".\n");
+            System.out.println("Betsy's Hitpoints are " + betsyHp + ".\n");
             System.out.println("Betsy the Spider: Your mother was a hamster and " +
                     "your father smelt of elderberries!\n");
 
             int heroAtk = 10;
-            int heroHealth = 100;
-            System.out.println(charName + ", your health is " + heroHealth + " and your trusty stick does "
+            int heroHp = 100;
+            int potions = 3;
+            int potionIncrease = 20;
+            System.out.println(charName + ", your health is " + heroHp + " and your trusty stick does "
                     + heroAtk + " damage! BOOYAH!");
+            System.out.println("You have " + potions + " potions\n");
+
+            
+            System.out.println("Prepare for Battle!!!");
+
+            System.out.println("What would you like to do?\n" +
+                    "1.Hit him with the stick\n" +
+                    "2.Drink the magic Elixir\n" +
+                    "3.Run for youe life!!!\n");
+            String playerOption = scan.nextLine();
+            
+            while(heroHp >= 0){
+                if(playerOption.equalsIgnoreCase("1")){
+                    betsyHp = betsyHp - heroAtk;
+                    System.out.println("You did " + heroAtk + " damage!, Bestsy Health is at " + betsyHp);
+                    break;
+                    
+                } else if(playerOption.equalsIgnoreCase("2")){
+                    System.out.println("You drank a potion and gained" + potionIncrease + " health");
+                    heroHp = heroHp + potionIncrease;
+                    System.out.println("Hero HP is at " + heroHp);
+                    break;
+                }
+            }
+
 
 
 
